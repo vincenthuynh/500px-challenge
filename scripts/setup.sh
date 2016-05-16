349d72a824eee8bf1a061eb83b3741761b871419
vincenthuynh <vinc3nt.huynh@gmail.com>
2016-05-16 00:44:15 -0400

    Revert "Revert "more packages, install golang, additional modules""
    This reverts commit 5ac3b6c0a3297d0e0b2c720b3294ca65482bf4c8.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,15 @@
 
 #Intialize
 apt-get update
+apt-get install -y build-essential
+apt-get install -y tcl8.5
+
+## Install golang | https://github.com/golang/go/wiki/Ubuntu
+yes | add-apt-repository ppa:ubuntu-lxc/lxd-stable
+apt-get install -y golang
 
 ## Install Puppet
 apt-get install -y puppet
+puppet module install puppetlabs-stdlib
+puppet module install puppetlabs-vcsrepo
+puppet module install jfryman-nginx
