602771d315243485da349ab5802defdfe544feb4
vincenthuynh <vinc3nt.huynh@gmail.com>
2016-05-17 11:22:02 -0400

    Fix dependency in proxy module
    https://github.com/jfryman/puppet-nginx/issues/610

--- modules/profile/manifests/proxy.pp
+++ modules/profile/manifests/proxy.pp
@@ -6,8 +6,9 @@ class profile::proxy (
   nginx::resource::vhost { 'roshi-server':
     listen_port    => 80,
     proxy          => 'http://localhost:6302',
+    require        => Class['::nginx::config']
   }
-
+  ->
   firewall { '101 whitelist subnet':
     proto       => 'tcp',
     destination => '172.31.32.0/25',
